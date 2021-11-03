78f8e48a9b277b5b409367ca83689da555b50cab
IronNand <eggersnd@etsu.edu>
2021-11-03 17:54:48 -0400

    moved Daniel's additions to the new tool

---
+++ StoredProcedures/getEvents.sql
@@ -0,0 +1,28 @@
+/*
+	getEvent vs getEvents:
+		getEvent - returns fine detail for a singular event
+		getEvents - returns less detail for a list of events to be used
+			to create tables or cards with high level detail
+*/
+
+--Switch to the wordpress database
+USE wordpress;
+
+--Create the new procedure
+DELIMITER ;;
+CREATE OR REPLACE PROCEDURE getEvents()
+BEGIN
+	select 	
+		event_id		as 'event_id', 
+		name	 		as 'name',
+		start_date		as 'start_date',
+		end_date		as 'end_date',
+		description		as 'description',
+		location		as 'location',
+		attendance		as 'attendance',
+		is_public		as 'is_public'
+	from Events;
+END;;
+DELIMITER ;
+
+
